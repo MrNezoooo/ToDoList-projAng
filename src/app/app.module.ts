b20dd8d9cea7c269e6182dcfaf03c49cb56404d0
Nazar Kinasevych <kin.naz@gmail.com>
2020-04-16 12:06:57 +0300

    виправив помилку

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {CategoriesComponent} from './views/categories/categories.component';
 import {TasksComponent} from './views/tasks/tasks.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 @NgModule({
   declarations: [
@@ -14,6 +17,12 @@ import {TasksComponent} from './views/tasks/tasks.component';
   ],
   imports: [
     BrowserModule,
+
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule,
+
     NoopAnimationsModule
   ],
   providers: [],
